f2e7c113938e32d70bad97d88d4d6b53a3d8a217
mcbianconi <murillo.bianconi@gmail.com>
2020-06-12 20:34:32 -0300

    melhorias no readm e config

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # Hackhaton CCR
 Projeto Frontend
+
+# Rodando a aplicação
+
+## Instalando o flutter
+[Página oficial](https://flutter.dev/docs/get-started/install)
+
+## Configurando o ambiente
+Baixar repositório
+```
+https://github.com/mcbianconi/hackathon-ccr-frontend.git
+```
+
+Entrar na pasta raiz
+```
+cd hackathon-ccr-frontend
+```
+
+Configurar o flutter
+```sh
+flutter channel beta
+flutter upgrade
+flutter config --enable-web
+flutter pub get
+flutter build web --release
+```
+
+## Rodando o app
+`flutter run -d chrome`
---
+++ lib/components/map.page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+const double CAMERA_ZOOM = 16;
+const double CAMERA_TILT = 80;
+const double CAMERA_BEARING = 30;
+const LatLng SOURCE_LOCATION = LatLng(42.747932, -71.167889);
+const LatLng DEST_LOCATION = LatLng(37.335685, -122.0605916);
+
+class MapPage extends StatefulWidget {
+  @override
+  _MapPageState createState() => _MapPageState();
+}
+
+class _MapPageState extends State<MapPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
